8456024461039fb10e1715901434a2e1c4230c1c
Ricardo <ricardo.mello@4d.com>
2014-02-20 11:33:59 +0100

    new syntax

--- widget.js
+++ widget.js
@@ -1,77 +1,82 @@
-WAF.define('JqxCombobox', function() {
-    "use strict";
+WAF.define('JqxCombobox', ['waf-core/widget'], function(widget) {
+    'use strict';
     // Restore the original version of jQuery
-    var $ = jQuery.noConflict( true );
+    var $ = jQuery.noConflict(true);
 
-    var widget = WAF.require('waf-core/widget');
-    var JqxCombobox = widget.create('JqxCombobox');
+    var JqxCombobox = widget.create('JqxCombobox', {
 
-    JqxCombobox.addProperty('items', {
-        type: 'datasource',
-        attributes: [{
-            name: 'value'
-        }, {
-            name: 'label'
-        }, {
-            name: 'html'
-        }]
-    });
+        source: widget.property({
+            type: 'datasource',
+            attributes: [{
+                name: 'value'
+            }, {
+                name: 'label'
+            }]
+        }),
 
-    JqxCombobox.addProperty('value');
+        searchMode: widget.property({
+            type: 'enum',
+            'values': {
+                none: 'none',
+                startswithignorecase: 'starts with ignore case',
+                startswith: 'starts with',
+                endswithignorecase: 'ends with ignore case',
+                endswith: 'endswith',
+                containsignorecase: 'contains ignore case',
+                contains: 'contains',
+                equalsignorecase: 'equals ignore case',
+                equals: 'equals'
+            },
+            bindable: false
+        }),
 
-    JqxCombobox.addProperty('searchMode', {
-        type: "enum",
-        "enum": {
-            startswithignorecase: "starts with ignore case",
-            startswith: "starts with",
-            endswithignorecase: "ends with ignore case",
-            endswith: "endswith",
-            containsignorecase: "contains ignore case",
-            contains: "contains",
-            equalsignorecase: "equals ignore case",
-            equals: "equals"
-        },
-        bindable: false
-    });
+        value: widget.property(),
 
-    JqxCombobox.addProperty('autoComplete', { type: 'boolean', bindable: false });
+        autoComplete: widget.property({
+            type: 'boolean',
+            bindable: false
+        }),
 
-    JqxCombobox.prototype.init = function() {
-        var $node = $(this.node);
-        $node.jqxComboBox({
-            source: [],
-            searchMode: this.searchMode(),
-            autoComplete: this.autoComplete()
-        });
-
-        this.value.onChange(function(value) {
-            $node.val(value);
-        });
-
-        $node.on('change', function (event) {
-            this.value($node.val());
-        }.bind(this));
-
-        this.items.onCollectionChange(function(elements) {
-            $node.jqxComboBox('clear');
-            elements.forEach(function(element) {
-                if(element.html === undefined) {
-                    element.html = element.label;
-                }
-                $node.jqxComboBox('addItem', element);
+        init: function() {
+            var self = this;
+            var $node = $(this.node);
+            $node.jqxComboBox({
+                source: [],
             });
-        });
-    };
 
-    JqxCombobox.prototype.open = function() {
-        $(this.node).jqxComboBox('open');
-    };
+            var subscriber = this.value.onChange(function(value) {
+                $node.val(value);
+            });
 
-    JqxCombobox.prototype.close = function() {
-        $(this.node).jqxComboBox('close');
-    };
+            $node.on('change', function() {
+                subscriber.pause();
+                self.value($node.val());
+                subscriber.resume();
+            });
+
+            this.source.onCollectionChange(function(elements) {
+                if (!elements.length) return;
+                $node.jqxComboBox({
+                    source: elements
+                });
+
+                $node.jqxComboBox({
+                    searchMode: this.searchMode(),
+                    autoComplete: this.autoComplete()
+                });         
+                    
+            });
+        },
+
+        open: function() {
+            $(this.node).jqxComboBox('open');
+        },
+
+        close: function() {
+            $(this.node).jqxComboBox('close');
+        }
+
+    });
 
     return JqxCombobox;
 });
-
-// For more information, refer to http://doc.wakanda.org/Wakanda0.DevBranch/help/Title/en/page3871.html
